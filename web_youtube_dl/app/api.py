20200910f04c41be7e63d7c0b2f9c8c6b13ffba9
Uriel Mandujano <uriel.mandujano14@gmail.com>
2020-10-03 22:14:55 -0500

    Adds FastAPI return schema validation

--- web_youtube_dl/app/api.py
+++ web_youtube_dl/app/api.py
@@ -2,14 +2,16 @@ import asyncio
 
 from fastapi import APIRouter, Form
 from fastapi.responses import FileResponse
-from web_youtube_dl.app.models import DownloadRequest
+from web_youtube_dl.app.models import DownloadRequest, DownloadResponse
 from web_youtube_dl.app.youtube_dl_helpers import download_file
 
 router = APIRouter()
 
 
 @router.post(
-    "/", description="Trigger an asynchronous file download",
+    "/",
+    description="Trigger an asynchronous file download",
+    response_model=DownloadResponse,
 )
 async def download(dl_req: DownloadRequest):
     loop = asyncio.get_running_loop()
