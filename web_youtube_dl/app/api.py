d51f4d544413bbf097f5934c189dd918ef5a4e38
Uriel Mandujano <5422638+konkolorado@users.noreply.github.com>
2024-02-25 19:01:03 -0600

    ft: refactor
    - use database as persitence layer
    - add sqlmodel to manage DB
    - use htmx for frontend
    - add versioneer for versioning
    - update python version
    - remove websocket support

--- web_youtube_dl/app/api.py
+++ web_youtube_dl/app/api.py
@@ -1,38 +1,77 @@
-import asyncio
 import logging
+import uuid
 
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, HTTPException
+from fastapi.responses import Response
 from pydantic import BaseModel, HttpUrl
 
-from web_youtube_dl.services import metadata, progress, youtube
+from web_youtube_dl.db import DownloadRepo, Request, RequestRepo, get_session
+from web_youtube_dl.services import metadata, youtube
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
-queues = progress.ProgressQueues()
-mm = metadata.MetadataManager()
-dlm = youtube.DownloadManager()
 
 
 class DownloadRequest(BaseModel):
     url: HttpUrl
 
 
-class DownloadResponse(BaseModel):
-    filename: str
+async def _download(id: uuid.UUID):
+    async with get_session() as session:
+        requests = RequestRepo(session)
+        if req := await requests.get_request(id):
+            downloads = DownloadRepo(session)
+            download = await downloads.get_download(req.download_url)
+            if download is None or download.audio is None:
+                ytd = youtube.YTDownload(req.download_url, id)
+                await youtube.DownloadManager(ytd).download_and_process(
+                    metadata.MetadataManager()
+                )
+            else:
+                await requests.complete_request(req)
 
 
-@router.post(
-    "/",
-    description="Trigger an asynchronous file download",
-    response_model=DownloadResponse,
+async def create_and_begin_download(url: str, tasks: BackgroundTasks) -> Request:
+    async with get_session() as session:
+        requests = RequestRepo(session)
+        request = await requests.create_request(url)
+        assert request.id is not None
+
+    tasks.add_task(_download, request.id)
+    return request
+
+
+async def get_download_request_or_404(session, req_id: uuid.UUID):
+    requests = RequestRepo(session)
+    if (request := await requests.get_request(req_id)) is None:
+        raise HTTPException(status_code=404, detail="Request ID not found")
+    return request
+
+
+@router.post("/", description="Trigger an async file download", response_model=Request)
+async def start_download(req: DownloadRequest, background_tasks: BackgroundTasks):
+    return await create_and_begin_download(str(req.url), background_tasks)
+
+
+@router.get(
+    "/{req_id}/status",
+    description="Retrieve a download's status",
+    response_model=Request,
 )
-async def download(req: DownloadRequest):
-    logger.info(f"Handling download request {req}")
-    ytd = youtube.YTDownload(req.url, pqs=queues)
-    queues.track(ytd.filename)
-    loop = asyncio.get_running_loop()
-    filepath = await loop.run_in_executor(
-        None, dlm.download_and_process, ytd, mm, queues
+async def get_download_status(req_id: uuid.UUID):
+    async with get_session() as session:
+        data = await get_download_request_or_404(session, req_id)
+        data.download.audio = None
+        return data
+
+
+@router.get("/{req_id}", description="Retrieve a downloaded file")
+async def get_download(req_id: uuid.UUID):
+    async with get_session() as session:
+        data = await get_download_request_or_404(session, req_id)
+
+    headers = {"Content-Disposition": f'attachment; filename="{data.download.url}"'}
+    return Response(
+        content=data.download.audio, media_type="audio/mpeg", headers=headers
     )
-    return {"filename": filepath.name}
